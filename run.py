79be12942d32d09bb9d829881c505b8fe0aa2de9
zahramousavi930 <zahramousavi930@gmail.com>
2022-08-28 16:23:11 +0000

    add new changes

--- run.py
+++ run.py
@@ -1,8 +1,77 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import string
+import os
+import sys
+from random import randint
 
 
+size = 0
+username = ''
+scores = scores = {"computer": 0, "player": 0};
+
+def random_spots(garid_size):
+    
+    x = randint(0, garid_size - 1)
+    y = randint(0, garid_size - 1)
+    return (x, y)
+
+
+def valid_spots(x, y, grid_size):
+   
+    if 0 <= x < grid_size and 0 <= y < grid_size:
+        return True
+
+    return False
+
+class Game_Board:
+    
+    def __init__(self, size, name, player=False):
+        self.size = size
+        self.num_ships = size
+        self.name = name
+        self.player = player
+        self.ships = []
+        self.guesses = []
+        self.populate()
+        
+    def print(self):
+        
+        print(f"{self.name}'s Game_Board:")
+        for row in self.board:
+            print(" ".join(row))
+
+    def guess(self, x, y):
+        
+        self.guesses.append((x, y))
+        self.board[x][y] = "X"
+
+        if (x, y) in self.ships:
+            self.board[x][y] = "*"
+            return True
+        else:
+            return False
+
+    def already_guessed(self, x, y):
+        
+        if (x, y) in self.guesses:
+            return True
+        return False
+
+    def last_guess(self):
+        
+        return self.guesses[-1]
+
+    def populate(self):
+       
+        board = [["." for x in range(self.size)] for y in range(self.size)]
+        self.board = board
+        for _ in range(self.num_ships):
+            x, y = random_spots(self.size)
+            while (x, y) in self.ships:
+                x, y = random_spots(self.size)
+            self.ships.append((x, y))
+            if self.player:
+                self.board[x][y] = "@"
 # View Game Guide
 def view_game_guide():
     view_guide = input(' >>> show game guides? (yes/no):\n')
@@ -34,5 +103,28 @@ GUIDES = """
 When prompted, type n (no).
 To exit the game, enter 'exit' at any time.
 """
+def enter_name():
+    global player_name
+    player_name = input(' >>> Put your name here:\n')
+    if player_name == 'exit':
+        end_game()
+
+    if player_name == '':
+        print('Name not entered.')
+        enter_name()
+    else:
+        if any(char in string.punctuation for char in player_name):
+            print('Name has a special character in it,'
+                  'Please only use letters.')
+            enter_name()
+        elif any(char.isdigit() for char in player_name):
+            print('Use only letters if the name contains a number.')
+            enter_name()
+        else:
+            select_board_size()
+
+            def end_game():
+    print('Thank you for playing')
+    sys.exit()
 
 view_game_guide();
